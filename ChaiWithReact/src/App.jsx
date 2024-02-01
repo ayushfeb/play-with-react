e949313ea218fbe22a24cd1e3c8fa31496375846
Ayush <53814109+ayushfeb@users.noreply.github.com>
2024-02-01 17:46:00 +0530

    Custom react made with the help of javaScript

---
+++ ChaiWithReact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+      <Chai></Chai>
+      <h1>Chai par Charcha with Ayush</h1>
+    </>
+  )
+}
+
+export default App
---
+++ ChaiWithReact/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai() {
+    return (
+      <>
+        <h1>Looking forward to kill in react</h1>
+      </>
+    )
+  }
+
+  export default Chai
